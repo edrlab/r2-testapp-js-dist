0231bc5ed20092b53aff69203410f4fcbeccd4a7
danielweck <daniel.weck@gmail.com>
2018-03-05 10:47:39 +0000

    add new dist

--- dist/es5/src/electron/renderer/index.js
+++ dist/es5/src/electron/renderer/index.js
@@ -382,6 +382,10 @@ var initFontSelector = function () {
             label: "Readable (dys)",
             style: "font-family: AccessibleDfa;",
         }, {
+            id: ID_PREFIX + "DUO",
+            label: "Duospace",
+            style: "font-family: \"IA Writer Duospace\", Consolas, monospace;",
+        }, {
             id: ID_PREFIX + "MONO",
             label: "Monospace",
             style: "font-family: \"Andale Mono\", Consolas, monospace;",
